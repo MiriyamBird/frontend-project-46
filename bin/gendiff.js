ddaa648d1b2e6f249f81ce5b3785840929d0cfee
Mariya Panska <miriyam_bird@icloud.com>
2022-11-30 16:07:19 +0300

    Step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ const program = new Command();
 
 program
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+    .version('1.0.0')
+    .argument('<filepath1>', 'path to file 1')
+    .argument('<filepath2>', 'path to file 2')
+    .option('-f, --format <type>', 'output format')
 
 program.parse();
