597352cb16c6e25c54a1ce00d7f1ceab053e4297
Mariya Panska <miriyam_bird@icloud.com>
2022-12-14 16:00:05 +0300

    Step 3. Dont work - try 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,9 +11,8 @@ program
     .arguments('<filepath1> <filepath2>')
     .option('-f, --format <type>', 'output format')
     .helpOption('-h, --help', 'output usage information')
+    .action((filepath1, filepath2) => {
+        console.log(genDiff(filepath1, filepath2));
+    });
 
 program.parse();
-
-program.action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
-});
--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 import { readFileSync } from 'node:fs';
 
 const config = (pathToFile1, pathToFile2) => {
-    const config1 = JSON.parse(fs.readFileSync(pathToFile1));
-    const config2 = JSON.parse(fs.readFileSync(pathToFile2));
+    const config1 = JSON.parse(readFileSync(pathToFile1));
+    const config2 = JSON.parse(readFileSync(pathToFile2));
 
     return [config1, config2];
 };
