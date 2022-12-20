f80c8fddc3138ff820e5d9a1df95887e6635be34
Mariya Panska <miriyam_bird@icloud.com>
2022-12-20 14:28:40 +0300

    Step 4. Dont work - try 2

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,5 @@
 import { readFileSync } from 'node:fs';
-import path from 'path';
-import { dirname } from 'path';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../index.js';
 
@@ -8,16 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-let json1;
-let json2;
-
-beforeAll(() => {
-    json1 = JSON.parse(readFile('file1.json'));
-    json2 = JSON.parse(readFile('file2.json'));
-});
-
-test ('check json', () => {
-    expect(genDiff(json1, json2)).toEqual();
+test('check json', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFileSync(getFixturePath('expected-result.txt', 'utf8')));
 });
