e837b53401181b176e0712af887b8f6b356f47b1
Mariya Panska <miriyam_bird@icloud.com>
2022-12-21 16:41:24 +0300

    Step 4. Work. Problems with lint

--- Makefile
+++ Makefile
@@ -13,5 +13,8 @@ install: #install
 jest: #jest
 	NODE_OPTIONS=--experimental-vm-modules npx jest
 
+coverage: #jest coverage
+	NODE_OPTIONS=--experimental-vm-modules npx jest --coverage
+
 gendiff: #gendiff
 	gendiff __fixtures__/file1.json __fixtures__/file2.json
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,5 +9,5 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('check json', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFileSync(getFixturePath('expected-result.txt', 'utf8')));
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFileSync(getFixturePath('expected-result.txt'), 'utf8'));
 });
