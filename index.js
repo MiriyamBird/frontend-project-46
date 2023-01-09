6964805bd754a00b3e7885e675b690a6eef72d4f
Mariya Panska <miriyam_bird@icloud.com>
2023-01-09 15:18:28 +0300

    Step 6

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import compareFiles from './src/comparison.js';
 import config from './src/config.js';
+import stylish from './src/formatters/stylish.js';
 
-const genDiff = (file1, file2) => compareFiles(config(file1), config(file2));
+const genDiff = (file1, file2) => stylish(compareFiles(config(file1), config(file2)));
 
 export default genDiff;
--- src/comparison.js
+++ src/comparison.js
@@ -1,46 +1,29 @@
 import _ from 'lodash';
 
-const checkKeysIsChanged = (file1, file2) => {
+const compareFiles = (file1, file2) => {
   const keys1 = Object.keys(file1);
   const keys2 = Object.keys(file2);
-  const keys = _.union(keys1, keys2);
+  const allKeys = _.union(keys1, keys2);
+  const sortedKeys = _.sortBy(allKeys);
 
-  const result = {};
-  for (const key of keys) {
+  const result = sortedKeys.map((key) => {
+    if (_.isObject(file1[key]) && _.isObject(file2[key])) {
+      return { type: 'object', key, value: compareFiles(file1[key], file2[key]) };
+    }
     if (!Object.hasOwn(file1, key)) {
-      result[key] = 'added';
-    } else if (!Object.hasOwn(file2, key)) {
-      result[key] = 'deleted';
-    } else if (file1[key] !== file2[key]) {
-      result[key] = 'changed';
-    } else {
-      result[key] = 'unchanged';
+      return { type: 'added', key, value: file2[key] };
     }
-  }
-
-  return result;
-};
-
-const compareFiles = (file1, file2) => {
-  const keysIsChanged = checkKeysIsChanged(file1, file2);
-  const allKeys = _.sortBy(Object.keys(keysIsChanged));
-
-  let result = '{\n';
-
-  for (const key of allKeys) {
-    if (keysIsChanged[key] === 'unchanged') {
-      result = `${result}    ${key}: ${file1[key]}\n`;
-    } else if (keysIsChanged[key] === 'deleted') {
-      result = `${result}  - ${key}: ${file1[key]}\n`;
-    } else if (keysIsChanged[key] === 'added') {
-      result = `${result}  + ${key}: ${file2[key]}\n`;
-    } else {
-      result = `${result}  - ${key}: ${file1[key]}\n`;
-      result = `${result}  + ${key}: ${file2[key]}\n`;
+    if (!Object.hasOwn(file2, key)) {
+      return { type: 'deleted', key, value: file1[key] };
     }
-  }
+    if (file1[key] !== file2[key]) {
+      return {
+        type: 'changed', key, oldValue: file1[key], newValue: file2[key],
+      };
+    }
 
-  result = `${result}}`;
+    return { type: 'changed', key, value: file1[key] };
+  });
 
   return result;
 };
