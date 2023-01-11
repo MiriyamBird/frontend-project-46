b001336ab44d50b25f73890f137af3ff037bd562
Mariya Panska <miriyam_bird@icloud.com>
2023-01-11 16:59:33 +0300

    Step 7. Fix lint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import compareFiles from './src/comparison.js';
 import config from './src/config.js';
-import formatType from './src/formatters/index.js';
+import format from './src/formatters/index.js';
 
-const genDiff = (file1, file2, style = stylish) => formatType((compareFiles(config(file1), config(file2))), style);
+const genDiff = (file1, file2, style = 'stylish') => format((compareFiles(config(file1), config(file2))), style);
 
 export default genDiff;
